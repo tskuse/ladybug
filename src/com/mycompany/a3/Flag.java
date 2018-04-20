39648d5f2df08a1f751cf48b7c0c11911ab5ed00
Tony Skuse <tskuse@gmail.com>
2018-04-19 23:12:41 -0700

    draw Flags

--- src/com/mycompany/a3/Flag.java
+++ src/com/mycompany/a3/Flag.java
@@ -1,11 +1,12 @@
 package com.mycompany.a3;
 
 import com.codename1.charts.util.ColorUtil;
+import com.codename1.ui.Graphics;
 import com.codename1.ui.geom.Point2D;
 
-public class Flag extends Fixed implements IUncolorable {
+public class Flag extends Fixed implements IDrawable, IUncolorable {
 
-    private static final int DEFAULT_SIZE = 10;
+    private static final int DEFAULT_SIZE = 60;
     private static final int DEFAULT_COLOR = ColorUtil.rgb(0, 0, 255);
 
     private int sequenceNumber;
@@ -39,5 +40,18 @@ public class Flag extends Fixed implements IUncolorable {
                 + "," + ColorUtil.blue(getColor()) + "]  size=" + getSize()
                 + " seqNum=" + getSequenceNumber();
     }
+
+    public void draw(Graphics g, Point2D pCmpRelPrnt) {
+        Point2D origin = new Point2D(pCmpRelPrnt.getX() + getLocation().getX(), pCmpRelPrnt.getY() + getLocation().getY());
+        Point2D v1 = new Point2D(origin.getX(), origin.getY() - (getSize() + 1) / 2);
+        Point2D v2 = new Point2D(origin.getX() - (getSize() + 1) / 2, origin.getY() + (getSize() + 1) / 2);
+        Point2D v3 = new Point2D(origin.getX() + (getSize() + 1) / 2, origin.getY() + (getSize() + 1) / 2);
+        g.setColor(getColor());
+        g.fillTriangle((int) v1.getX(), (int) v1.getY(), (int) v2.getX(), (int) v2.getY(), (int) v3.getX(), (int) v3.getY());
+        g.setColor(ColorUtil.rgb(255, 255, 255));
+        g.drawStringBaseline(Integer.toString(sequenceNumber),
+                             (int) origin.getX() - (g.getFont().stringWidth(Integer.toString(sequenceNumber)) + 1) / 2,
+                             (int) origin.getY() + (g.getFont().getHeight() + 1) / 2);
+	}
 
 }
