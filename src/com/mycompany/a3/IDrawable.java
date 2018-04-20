1f78cf36477368a5d9fecbda28350ac753d5e30a
Tony Skuse <tskuse@gmail.com>
2018-04-19 19:19:40 -0700

    Adds IDrawable interface

---
+++ src/com/mycompany/a3/IDrawable.java
@@ -0,0 +1,8 @@
+package com.mycompany.a3;
+
+import com.codename1.ui.Graphics;
+import com.codename1.ui.geom.Point2D;
+
+public interface IDrawable {
+    public void draw(Graphics g, Point2D pCmpRelPrnt);
+}
