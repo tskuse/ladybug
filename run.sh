12b728e26b4ceb92938b7ad92e9e5e35371de935
Tony Skuse <tskuse@gmail.com>
2018-04-22 12:40:30 -0700

    build name updates

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp dist/A2Prj.jar:JavaSE.jar com.codename1.impl.javase.Simulator com.mycompany.a3.Starter
+java -cp dist/A3Prj.jar:JavaSE.jar com.codename1.impl.javase.Simulator com.mycompany.a3.Starter
